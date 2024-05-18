c53f53049222997197b0dbbe14688f21d9c983c4
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-18 23:00:11 +0200

    feat: add App state use and list display ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import Card from './components/Card'
+import { library } from './data/books.json'
 
 function App() {
-  return <></>
+  const [selectedList, updateList] = useState<string[]>([])
+
+  const selectCard = (key: string) => {
+    updateList([...selectedList, key])
+  }
+
+  const removeCard = (key: string) => {
+    const oldList = [...selectedList]
+    oldList.splice(selectedList.indexOf(key), 1)
+    updateList(oldList)
+  }
+
+  useEffect(() => {
+    console.log(selectedList)
+  }, [selectedList])
+
+  return (
+    <>
+      {library.map((item) => (
+        <Card
+          img={item.book.cover}
+          addInReadingList={selectCard}
+          removeFromReadingList={removeCard}
+          key={item.book.title}
+          id={item.book.title}
+        />
+      ))}
+    </>
+  )
 }
 
 export default App
