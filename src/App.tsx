5b01ad0d163b9f1aa5ac9d1b719c1eeb8f29957c
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-19 00:42:14 +0200

    feat: added container and selected list div

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
-import './App.css'
+import './App.scss'
 import Card from './components/Card'
 import { library } from './data/books.json'
+// import { animate } from 'motion'
 
 function App() {
   const [selectedList, updateList] = useState<string[]>([])
@@ -21,17 +22,33 @@ function App() {
   }, [selectedList])
 
   return (
-    <>
-      {library.map((item) => (
-        <Card
-          img={item.book.cover}
-          addInReadingList={selectCard}
-          removeFromReadingList={removeCard}
-          key={item.book.title}
-          id={item.book.title}
-        />
-      ))}
-    </>
+    <div className="container-fluid row">
+      <div
+        className={
+          selectedList.length > 0
+            ? 'row d-flex flex-wrap ali col-sm-10 non-selected'
+            : 'row non-selected'
+        }
+      >
+        {library.map((item) => (
+          <Card
+            img={item.book.cover}
+            addInReadingList={selectCard}
+            removeFromReadingList={removeCard}
+            key={item.book.title}
+            id={item.book.title}
+          />
+        ))}
+      </div>
+      {selectedList.length > 0 ? (
+        <div className="reading-list col-sm-2 ">
+          <h2>Reading-List</h2>
+          <hr className="dashed" />
+        </div>
+      ) : (
+        ''
+      )}
+    </div>
   )
 }
 
