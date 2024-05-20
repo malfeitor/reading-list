1029a3dadc69338ee3b738b351b79df7dfc7c3f4
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-20 03:09:23 +0200

    fix: correct TS errors 🐛

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { useRef, useState } from 'react'
 import './App.scss'
-// import Card from './components/Card'
 import { library } from './data/books.json'
 import { animate } from 'motion'
 
@@ -8,19 +7,19 @@ const bookList = library.map((item) => item.book.cover)
 
 export default function App() {
   const [choosen, setChoosen] = useState([...bookList.map(() => false)])
-  const bookRefs = useRef([])
-  const notChoosenDiv = useRef(null)
-  const choosenDiv = useRef(null)
+  const bookRefs = useRef<HTMLImageElement[]>([])
+  const notChoosenDiv = useRef<HTMLDivElement>(null)
+  const choosenDiv = useRef<HTMLDivElement>(null)
 
-  function toggleBook(index) {
+  function toggleBook(index: number) {
     console.log(bookRefs)
     const oldRect = bookRefs.current.map((book) => book.getBoundingClientRect())
     if (choosen[index]) {
-      choosenDiv.current.removeChild(bookRefs.current[index])
-      notChoosenDiv.current.append(bookRefs.current[index])
+      choosenDiv.current!.removeChild(bookRefs.current[index])
+      notChoosenDiv.current!.append(bookRefs.current[index])
     } else {
-      notChoosenDiv.current.removeChild(bookRefs.current[index])
-      choosenDiv.current.append(bookRefs.current[index])
+      notChoosenDiv.current!.removeChild(bookRefs.current[index])
+      choosenDiv.current!.append(bookRefs.current[index])
     }
     const newChoosen = [...choosen]
     newChoosen[index] = !newChoosen[index]
@@ -35,7 +34,14 @@ export default function App() {
     })
   }
 
-  function computeAnimation({ elem, oldRect, newRect }) {
+  type ComputeAnimationProps = {
+    elem: HTMLElement
+    oldRect: DOMRect
+    newRect: DOMRect
+  }
+
+  function computeAnimation({ elem, oldRect, newRect }: ComputeAnimationProps) {
+    console.log(oldRect)
     const translate = { x: oldRect.x - newRect.x, y: oldRect.y - newRect.y }
     // first we set it at his old place
     animate(elem, { x: translate.x, y: translate.y }, { duration: 0 })
@@ -48,7 +54,7 @@ export default function App() {
       <div className="not-choosen" ref={notChoosenDiv}>
         {bookList.map((book, index) => (
           <img
-            className="col-sm-1"
+            className="col-sm-1 book"
             src={book}
             ref={(e) => (bookRefs.current[index] = e)}
             onClick={() => toggleBook(index)}
