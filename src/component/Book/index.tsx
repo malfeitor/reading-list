074766124ff997ab447aac5228a33f36fa2cf627
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-21 22:55:57 +0200

    feat: add style and animation type ✨

--- src/component/Book/index.tsx
+++ src/component/Book/index.tsx
@@ -1,13 +1,25 @@
-import { Col, Image } from 'react-bootstrap'
+import { motion } from 'framer-motion'
+import { Col } from 'react-bootstrap'
+
+type CSSObjectType = {
+  [key: string]: string
+}
 
 type BookProps = {
   img: string
+  style: CSSObjectType | void
+  animation: CSSObjectType | void
 }
 
-export default function Book({ img }: BookProps) {
+export default function Book({ img, style, animation }: BookProps) {
   return (
     <Col>
-      <Image src={img} className="book rounded-4" />
+      <motion.img
+        src={img}
+        className="book rounded-4"
+        style={style as CSSObjectType}
+        animate={animation as CSSObjectType}
+      />
     </Col>
   )
 }
