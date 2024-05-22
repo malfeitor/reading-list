7af82c38fb068035a2a591e14b09935db006ae02
Clement Pivot <8830707+malfeitor@users.noreply.github.com>
2024-05-22 02:39:12 +0200

    refactor: use animation keyframes 📦

--- src/utils/animations.ts
+++ src/utils/animations.ts
@@ -9,10 +9,12 @@ type ComputeAnimationProps = {
 
 export function moveBooks({ elem, oldRect, newRect }: ComputeAnimationProps) {
   const translate = { x: oldRect.x - newRect.x, y: oldRect.y - newRect.y }
-  // first we set it at his old place
-  animate(elem, { x: translate.x, y: translate.y }, { duration: 0 })
-  // then we move it
-  animate(elem, { x: 0, y: 0, scale: 1 }, { duration: 1 })
+  // translate from old place to new one
+  animate(
+    elem,
+    { x: [translate.x, 0], y: [translate.y, 0], scale: 1 },
+    { duration: 1 }
+  )
 }
 
 export function updateReadingListBooksSize(
